d0c6175998220ef50b72c7f2d4f858612c7b27ec
Vnnovate Solutions <git@vnnovate.com>
2017-07-19 21:28:35 +0530

    Add files via upload

---
+++ custompolling.sql
@@ -0,0 +1,33 @@
+-- --------------------------------------------------------
+-- Host:                         vnnovate.com
+-- Server version:               5.6.32-78.1 - Percona Server (GPL), Release 78.1, Revision 8bb53b6
+-- Server OS:                    Linux
+-- HeidiSQL Version:             9.4.0.5125
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for vnnovate_custom_poll
+DROP DATABASE IF EXISTS `vnnovate_custom_poll`;
+CREATE DATABASE IF NOT EXISTS `vnnovate_custom_poll` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `vnnovate_custom_poll`;
+
+-- Dumping structure for table vnnovate_custom_poll.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `polling_point` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `vote` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
