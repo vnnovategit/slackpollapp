f09e87fcd49f718901912fbff6ea2225796486d2
Vnnovate Solutions <git@vnnovate.com>
2017-07-20 21:02:47 +0530

    Add files via upload

--- custompolling.sql
+++ custompolling.sql
@@ -13,19 +13,27 @@
 
 
 -- Dumping database structure for vnnovate_custom_poll
-DROP DATABASE IF EXISTS `vnnovate_custom_poll`;
 CREATE DATABASE IF NOT EXISTS `vnnovate_custom_poll` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
 USE `vnnovate_custom_poll`;
 
+-- Dumping structure for table vnnovate_custom_poll.pollmaster
+CREATE TABLE IF NOT EXISTS `pollmaster` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `gained_vote` varchar(250) COLLATE utf8_unicode_ci DEFAULT '0',
+  `poll_details` text COLLATE utf8_unicode_ci,
+  `is_deleted` int(11) NOT NULL DEFAULT '0' COMMENT '0=>No,1=>Yes',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- Data exporting was unselected.
 -- Dumping structure for table vnnovate_custom_poll.users
-DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `polling_point` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `vote` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=17 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
