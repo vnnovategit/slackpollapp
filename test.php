11fbe1954f8418383c9761367ce57db57fe4f5cc
Vnnovate Solutions <git@vnnovate.com>
2017-07-20 21:03:04 +0530

    Add files via upload

--- test.php
+++ test.php
@@ -53,69 +53,131 @@ $ch = curl_init();
 $request_params = explode('"',$text);
 
 if(trim($request_params[0])=="create"){
-	$request_params[2] = trim($request_params[2]);
-	$optionsArry = explode("--",$request_params[2]);
-	array_shift($optionsArry);
 
-	//create poll functionality will be started here...
-	$payload = [
- 	"channel"=>$_POST['channel_name'],
- 	"text"=>"@".$_POST['user_name']." created a new poll! Vote in it!",
-    "link_names"=> 1,
-    "attachments"=> [
-            [
-                "fallback"=> "@".$_POST['user_name']." created a new poll! Vote in it!",
+	//check here there are poll open or not if opened then first need to close poll
+	$sqlPoll = "SELECT * from pollmaster WHERE is_deleted='0'";
+	$stmtPoll = $conn->prepare($sqlPoll); 
+	$stmtPoll->execute(); 
+	$rowPoll = $stmtPoll->fetch();
 
-                "color"=> "good",
-                "mrkdwn_in"=> ["fields", "text"],
-                "fields"=> [
-                    [
-                        "title"=> $request_params[1],
-                        "value"=> ""
-                    ]
-                ]
-            ]
-        ]
-    ];
+	if($rowPoll){
+		//then you can not create new poll unless close current poll
+		$payload = array("text"=>"@ ".$_POST['user_name']." You can't create poll unless close current poll \n\nHow do I close poll? `/setpoll close`");
+
+	}else{
+		//go ahead you can create new poll
+		$request_params[2] = trim($request_params[2]);
+		$optionsArry = explode("--",$request_params[2]);
+		array_shift($optionsArry);
+
+		//create poll functionality will be started here...
+		$payload = [
+	 	"channel"=>$_POST['channel_name'],
+	 	"text"=>"@".$_POST['user_name']." created a new poll! Vote in it!",
+	    "link_names"=> 1,
+	    "attachments"=> [
+	            [
+	                "fallback"=> "@".$_POST['user_name']." created a new poll! Vote in it!",
+
+	                "color"=> "good",
+	                "mrkdwn_in"=> ["fields", "text"],
+	                "fields"=> [
+	                    [
+	                        "title"=> $request_params[1],
+	                        "value"=> ""
+	                    ]
+	                ]
+	            ]
+	        ]
+	    ];
 
 
-	foreach ($optionsArry as $key => $value) {
-		$payload["attachments"][0]["fields"][0]["value"] .= "(".($key + 1).")".$value."\n";
+		foreach ($optionsArry as $key => $value) {
+			$payload["attachments"][0]["fields"][0]["value"] .= "(".($key + 1).")".$value."\n";
+		}
+
+		$payload["attachments"][0]["fields"][0]["value"] .= "\n\nHow do I vote? `/setpoll cast [option number]`";	
+
+		//insert create poll here
+	    $gainedVote 	= 0;
+	    $pollDetails  	= $request_params[1]." ".$request_params[2]; 
+	    $isDeleted 		= 0;
+	    $sqlIns 		= "INSERT into pollmaster (gained_vote,poll_details,is_deleted) VALUES ('$gainedVote','$pollDetails','$isDeleted')";
+		$result 		= $conn->query($sqlIns);
+
 	}
 
-	$payload["attachments"][0]["fields"][0]["value"] .= "\n\nHow do I vote? `/setpoll cast [option number]`";	
+
+	
 }elseif(trim($request_params[0])=="cast"){
 	
 	//cast functionality will be started here..
 
-	//here first check user is exist or not if not then create and assign some default point 10000
-	$newUserName = $_POST['user_name'];
-	
-	$sql = "SELECT * from users WHERE username='$newUserName'";
-	$stmt = $conn->prepare($sql); 
-	$stmt->execute(); 
-	$row = $stmt->fetch();
+	//first check there are any active poll or not 
+	$sqlPoll = "SELECT * from pollmaster WHERE is_deleted='0'";
+	$stmtPoll = $conn->prepare($sqlPoll); 
+	$stmtPoll->execute(); 
+	$rowPoll = $stmtPoll->fetch();
 
-	if($row){
-    	// output data of each row
-	    $userid = $row["id"];
-        $newVote = $row["vote"]+1;
-        $newPoints = ($row["polling_point"]-($newVote*$newVote)); 
-        $updateSql = "UPDATE users SET vote=$newVote,polling_point=$newPoints WHERE id=$userid";
-        $result = $conn->query($updateSql);
-	} else {
-		//insert new users here
-	    $newVote 	 = 1;
-	    $newPoints = (10000-($newVote)); 
-	    $sqlIns = "INSERT into users (username,polling_point,vote) VALUES ('$newUserName','$newPoints','$newVote')";
-		$result = $conn->query($sqlIns);
+	if($rowPoll){
+		//here first check user is exist or not if not then create and assign some default point 10000
+		$newUserName = $_POST['user_name'];
+		
+		$sql = "SELECT * from users WHERE username='$newUserName'";
+		$stmt = $conn->prepare($sql); 
+		$stmt->execute(); 
+		$row = $stmt->fetch();
+
+		if($row){
+	    	// output data of each row
+		    $userid = $row["id"];
+	        $newVote = $row["vote"]+1;
+	        $newPollGainedPoints = ($newVote*$newVote);
+	        $newPoints = ($row["polling_point"]-($newVote*$newVote)); 
+	        $updateSql = "UPDATE users SET vote=$newVote,polling_point=$newPoints WHERE id=$userid";
+	        $result = $conn->query($updateSql);
+		} else {
+			//insert new users here
+		    $newVote 	 = 1;
+		    $newPoints = (10000-($newVote));
+		    $newPollGainedPoints = ($newVote*$newVote); 
+		    $sqlIns = "INSERT into users (username,polling_point,vote) VALUES ('$newUserName','$newPoints','$newVote')";
+			$result = $conn->query($sqlIns);
+		}
+	    
+	    //here updated or insert gained point in pollmaster
+	    $newPollPoints = ($rowPoll["gained_vote"]+$newPollGainedPoints); 
+	    $pollId 	   = $rowPoll["id"];		
+        $updatePollSql = "UPDATE pollmaster SET gained_vote=$newPollPoints WHERE id=$pollId";
+        $result = $conn->query($updatePollSql);
+		
+		//$conn->close();
+		$payload = array("text"=>"Thanks for voting @ ".$_POST['user_name']." your remaining points are $newPoints.");
+	}else{
+		// sorry you can't vote because ther are not any active poll
+		$payload = array("text"=>"You are not able to vote because of no any active poll found @ ".$_POST['user_name']);
 	}
-    
-	
-	//$conn->close();
-	$payload = array("text"=>"Thanks for voting @ ".$_POST['user_name']." your remaining points are $newPoints.");
 	//cast functionality will be end here...
 
+}elseif (trim($request_params[0])=="close") {
+	//close poll that means soft delete 
+	$is_deleted = 1;
+    $updateSql = "UPDATE pollmaster SET is_deleted=1";
+    $result = $conn->query($updateSql); 
+    $payload = array("text"=>"@ ".$_POST['user_name']."You have successfully closed poll.");
+}elseif (trim($request_params[0])=="status") {
+	//check there are any open or active poll or not 
+	
+	$sqlPoll = "SELECT * from pollmaster WHERE is_deleted='0'";
+	$stmtPoll = $conn->prepare($sqlPoll); 
+	$stmtPoll->execute(); 
+	$rowPoll = $stmtPoll->fetch();
+
+	if($rowPoll){
+		$payload = array("text"=>"@ ".$_POST['user_name']."Yes there are one active poll.");
+	}else{
+		$payload = array("text"=>"@ ".$_POST['user_name']."Sorry there are no any active poll.");
+	}
 }
 
 
